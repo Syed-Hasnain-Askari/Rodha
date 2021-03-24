7a1df574a8ae94c3f93a44ea3cbf97b182c818d0
Najaf Rizvi <hasnainaskari32@gmail.com>
2021-03-24 23:17:35 +0500

    changes in login branch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Login from './Login/Login'
 import './App.css';
 function App() {
   return (
     <div className="App">
       <h1>Heollow World</h1>
       <h1>We are in login branch</h1>
+      <Login/>
     </div>
   );
 }
