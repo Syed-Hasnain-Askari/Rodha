171ed41b625e25779dd3edcf0f130691dc639a6f
Najaf Rizvi <hasnainaskari32@gmail.com>
2021-03-24 23:35:02 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import Login from './component/Login'
-import Signup from './component/Signup'
 import './App.css';
 function App() {
   return (
     <div className="App">
       <Login/>
-      <Signup/>
     </div>
   );
 }
