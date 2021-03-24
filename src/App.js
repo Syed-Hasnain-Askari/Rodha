326c7ca139854e24e46ee35f92a453b0d42d5f1d
Najaf Rizvi <hasnainaskari32@gmail.com>
2021-03-25 01:48:21 +0500

    changes in master

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
-import Login from './component/Login'
+import Login from './Component/Login'
+import Signup from './Component/Signup'
 import './App.css';
 function App() {
   return (
     <div className="App">
       <Login/>
+      <Signup/>
     </div>
   );
 }
