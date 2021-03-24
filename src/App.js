c979d8bd18549b36a031c45a3461c27d3be23e76
Najaf Rizvi <hasnainaskari32@gmail.com>
2021-03-24 22:50:20 +0500

    chanegs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Login from './component/Login'
 import './App.css';
 function App() {
   return (
     <div className="App">
       <h1>Heollow World</h1>
+      <Login/>
     </div>
   );
 }
