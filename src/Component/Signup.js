c6590cbfbd6f5b641a29b6af071f7c55a51697ff
Najaf Rizvi <hasnainaskari32@gmail.com>
2021-03-24 23:36:40 +0500

    changes

--- src/Component/Signup.js
+++ src/Component/Signup.js
@@ -3,8 +3,7 @@ import React from 'react'
 export default function Signup() {
     return (
         <div>
-            <h1>This is Signup Section</h1>
-            <h1>This is Signup Component</h1>
+            
         </div>
     )
 }
